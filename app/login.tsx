46f9550d717486315925d9d0fef6dc07acb80af3
Aman Singh <amanaspirantsingh@gmail.com>
2026-07-15 23:23:50 +0530

    enhanced the form and validations and added keyboardAvoidingView, pressables and ScrollView for long forms

--- app/login.tsx
+++ app/login.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Pressable, Text, TextInput, View } from "react-native";
+import { Keyboard, KeyboardAvoidingView, Pressable, ScrollView, Text, TextInput, View } from "react-native";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -8,8 +8,12 @@ export default function Login() {
   const [status, setStatus] = useState('');
 
   const handleSubmit = () => {
-    if (!email.trim() && !password) {
-      setStatus("Email and Password are Required !");
+    if (!email.trim()) {
+      setStatus("Email is Required !");
+      return;
+    }
+    if (!password) {
+      setStatus("Password is Required !");
       return;
     }
     if (!/\S+@\S+\.\S+/.test(email)) {
@@ -25,43 +29,52 @@ export default function Login() {
   }
 
   return (
-    <View className="flex-1 justify-center items-center">
-      <View className="w-sm p-5 rounded-2xl">
-        <Text className="text-2xl font-bold text-center">DevConnect</Text>
-        <View className="border-2 p-5 rounded-2xl mt-2">
-          <Text className="text-xl font-bold text-center pt-2 mb-5">Login</Text>
+    <KeyboardAvoidingView behavior="padding" className="flex-1 justify-center items-center">
+      {/* we can use both TouchableWithoutFeedback or Pressable both in forms*/}
+      <Pressable onPress={()=>Keyboard.dismiss()}>
+        <View className="w-sm p-5 rounded-2xl">
+          <Text className="text-2xl font-bold text-center">DevConnect</Text>
+          <View className="border-2 p-5 rounded-2xl mt-2">
+            <Text className="text-xl font-bold text-center pt-2 mb-5">Login</Text>
 
-          <View className="flex gap-5">
-            <TextInput
-              placeholder="Email"
-              value={email}
-              onChangeText={setEmail}
-              keyboardType="email-address"
-              autoCapitalize="none"
-              autoCorrect={false}
-              className="border p-3 rounded-xl"
-            />
+            <View className="flex gap-5">
+              <TextInput
+                placeholder="Email"
+                value={email}
+                onChangeText={(e) => {
+                  setEmail(e);
+                  setStatus("");
+                }}
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCorrect={false}
+                className="border p-3 rounded-xl"
+              />
 
-            <TextInput
-              placeholder="Password"
-              value={password}
-              onChangeText={setPassword}
-              secureTextEntry
-              autoCapitalize="none"
-              autoCorrect={false}
-              className="border p-3 rounded-xl"
-            />
+              <TextInput
+                placeholder="Password"
+                value={password}
+                onChangeText={(e) => {
+                  setEmail(e);
+                  setStatus("");
+                }}
+                secureTextEntry
+                autoCapitalize="none"
+                autoCorrect={false}
+                className="border p-3 rounded-xl"
+              />
 
-            {status && <Text>{status}</Text>}
+              {status && <Text>{status}</Text>}
 
-            <Pressable onPress={handleSubmit} className="rounded-xl items-center bg-sky-500 p-3 ">
-              <Text className="text-white font-bold text-md">
-                Login
-              </Text>
-            </Pressable>
+              <Pressable onPress={handleSubmit} className="rounded-xl items-center bg-sky-500 p-3 ">
+                <Text className="text-white font-bold text-md">
+                  Login
+                </Text>
+              </Pressable>
+            </View>
           </View>
         </View>
-      </View>
-    </View>
+      </Pressable>
+    </KeyboardAvoidingView>
   )
 }
